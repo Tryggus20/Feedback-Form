c73be2b39097947967f640080eccb00e8dea245f
David Campbell <tryggus20@gmail.com>
2023-09-29 16:01:05 -0500

    set up index.js and added routes to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/feeling" exact></Route>
+          <Route path="/understanding" exact></Route>
+          <Route path="/support"></Route>
+          <Route path="/comments" exact></Route>
+          <Route path="/review" exact></Route>
+          <Route path="/thankyou" exact></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// trying just 1 reducer to rule them all!
+const feedbackReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_FEELING":
+      return [...state, action.payload];
+    case "ADD_UNDERSTANDING":
+      return [...state, action.payload];
+    case "ADD_SUPPORT":
+      return [...state, action.payload];
+    case "ADD_COMMENTS":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+const store= createStore(
+    feedbackReducer)
+
+
+ReactDOM.render(
+  <Provider store={store}>,
+    <App />, 
+  </Provider>,
+    document.getElementById('root')
+    );
+    registerServiceWorker();
