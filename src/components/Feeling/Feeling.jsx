59342467d9e2bf6c9131876f73e5f07d16e39403
David Campbell <tryggus20@gmail.com>
2023-11-05 16:07:27 -0600

    adding react-bootstrap

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,6 +1,8 @@
 import { useHistory } from "react-router-dom";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Form, Row, Col, InputGroup } from 'react-bootstrap';
+
 
 export default function Feeling() {
   const dispatch = useDispatch();
@@ -18,21 +20,39 @@ export default function Feeling() {
       alert("Please fill in the input field before continuing.");
     }
   };
-  const handleFeelingChange = (event) => {
-    setFeeling(event.target.value);
+  
+  const handleRatingChange = (event) => {
+    const newRating = parseInt(event.target.value, 10);
+    setFeeling(newRating);
   };
   return (
     <>
       <h1>How are you feeling today?</h1>
       <br />
-      <p>Feeling?</p>
-      <input
-        type="number"
-        min={1}
-        max={5}
-        value={feeling}
-        onChange={handleFeelingChange}
-      />
+      <Form>
+      <Form.Group as={Row} controlId="formRating">
+
+      <Form.Label column sm={3}>
+          Feeling (1-5):
+        </Form.Label>
+        <Col sm={9}>
+          <InputGroup>
+            {[1, 2, 3, 4, 5].map((value) => (
+              <InputGroup.Radio
+                key={value}
+                name="feeling"
+                id={`feeling-${value}`}
+                value={value}
+                checked={feeling === value}
+                onChange={handleRatingChange}
+              />
+            ))}
+            <p>{feeling}</p>
+          </InputGroup>
+        </Col>
+        </Form.Group>
+      </Form>
+      <br />
       <button type="button" onClick={addFeeling}>
         NEXT
       </button>
